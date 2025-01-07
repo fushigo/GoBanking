ef13cf707e1f6cde52c7c9d9f28b01ea0a67b52e
iKzuu <anangsetiaji30@gmail.com>
2025-01-07 15:58:14 +0700

    delete popup

--- app/GoBanking/AddSavingsFunds.h
+++ app/GoBanking/AddSavingsFunds.h
@@ -38,18 +38,28 @@ namespace GoBanking {
 	private: System::Windows::Forms::Panel^ transferInputPanel;
 	private: System::Windows::Forms::Button^ btnAddSavings;
 
-	private: System::Windows::Forms::Panel^ panel4;
-	private: System::Windows::Forms::Panel^ panel3;
-	private: System::Windows::Forms::Panel^ panel7;
-	private: System::Windows::Forms::Label^ pin;
-	private: System::Windows::Forms::TextBox^ pinInput;
-	private: System::Windows::Forms::Label^ nominalSavings;
 
-	private: System::Windows::Forms::TextBox^ addSavingsInput;
+
+	private: System::Windows::Forms::Panel^ panel7;
+
+
+
+
+
 
 	private: System::Windows::Forms::Label^ customerAccNum;
 	private: System::Windows::Forms::TextBox^ customerAccInput;
 	private: System::Windows::Forms::Label^ withdrw;
+	private: System::Windows::Forms::Panel^ nominalInputPanel;
+	private: System::Windows::Forms::Label^ label2;
+	private: System::Windows::Forms::Label^ nominalSavings;
+	private: System::Windows::Forms::TextBox^ addSavingsInput;
+	private: System::Windows::Forms::Panel^ panel1;
+	private: System::Windows::Forms::Panel^ pinInputPanel;
+	private: System::Windows::Forms::Label^ pinLabel;
+
+	private: System::Windows::Forms::TextBox^ pinInput;
+	private: System::Windows::Forms::Panel^ panel2;
 	protected:
 
 	private:
@@ -68,18 +78,23 @@ namespace GoBanking {
 			this->addSavingsMainPanel = (gcnew System::Windows::Forms::Panel());
 			this->transferInputPanel = (gcnew System::Windows::Forms::Panel());
 			this->btnAddSavings = (gcnew System::Windows::Forms::Button());
-			this->panel4 = (gcnew System::Windows::Forms::Panel());
-			this->panel3 = (gcnew System::Windows::Forms::Panel());
 			this->panel7 = (gcnew System::Windows::Forms::Panel());
-			this->pin = (gcnew System::Windows::Forms::Label());
-			this->pinInput = (gcnew System::Windows::Forms::TextBox());
-			this->nominalSavings = (gcnew System::Windows::Forms::Label());
-			this->addSavingsInput = (gcnew System::Windows::Forms::TextBox());
 			this->customerAccNum = (gcnew System::Windows::Forms::Label());
 			this->customerAccInput = (gcnew System::Windows::Forms::TextBox());
 			this->withdrw = (gcnew System::Windows::Forms::Label());
+			this->nominalInputPanel = (gcnew System::Windows::Forms::Panel());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->nominalSavings = (gcnew System::Windows::Forms::Label());
+			this->addSavingsInput = (gcnew System::Windows::Forms::TextBox());
+			this->panel1 = (gcnew System::Windows::Forms::Panel());
+			this->pinInputPanel = (gcnew System::Windows::Forms::Panel());
+			this->pinLabel = (gcnew System::Windows::Forms::Label());
+			this->pinInput = (gcnew System::Windows::Forms::TextBox());
+			this->panel2 = (gcnew System::Windows::Forms::Panel());
 			this->addSavingsMainPanel->SuspendLayout();
 			this->transferInputPanel->SuspendLayout();
+			this->nominalInputPanel->SuspendLayout();
+			this->pinInputPanel->SuspendLayout();
 			this->SuspendLayout();
 			// 
 			// addSavingsMainPanel
@@ -95,14 +110,10 @@ namespace GoBanking {
 			// 
 			// transferInputPanel
 			// 
+			this->transferInputPanel->Controls->Add(this->pinInputPanel);
+			this->transferInputPanel->Controls->Add(this->nominalInputPanel);
 			this->transferInputPanel->Controls->Add(this->btnAddSavings);
-			this->transferInputPanel->Controls->Add(this->panel4);
-			this->transferInputPanel->Controls->Add(this->panel3);
 			this->transferInputPanel->Controls->Add(this->panel7);
-			this->transferInputPanel->Controls->Add(this->pin);
-			this->transferInputPanel->Controls->Add(this->pinInput);
-			this->transferInputPanel->Controls->Add(this->nominalSavings);
-			this->transferInputPanel->Controls->Add(this->addSavingsInput);
 			this->transferInputPanel->Controls->Add(this->customerAccNum);
 			this->transferInputPanel->Controls->Add(this->customerAccInput);
 			this->transferInputPanel->Location = System::Drawing::Point(76, 175);
@@ -124,22 +135,6 @@ namespace GoBanking {
 			this->btnAddSavings->Text = L"Tabung";
 			this->btnAddSavings->UseVisualStyleBackColor = false;
 			// 
-			// panel4
-			// 
-			this->panel4->BackColor = System::Drawing::Color::White;
-			this->panel4->Location = System::Drawing::Point(27, 272);
-			this->panel4->Name = L"panel4";
-			this->panel4->Size = System::Drawing::Size(200, 5);
-			this->panel4->TabIndex = 34;
-			// 
-			// panel3
-			// 
-			this->panel3->BackColor = System::Drawing::Color::White;
-			this->panel3->Location = System::Drawing::Point(27, 194);
-			this->panel3->Name = L"panel3";
-			this->panel3->Size = System::Drawing::Size(450, 5);
-			this->panel3->TabIndex = 33;
-			// 
 			// panel7
 			// 
 			this->panel7->BackColor = System::Drawing::Color::White;
@@ -147,59 +142,6 @@ namespace GoBanking {
 			this->panel7->Name = L"panel7";
 			this->panel7->Size = System::Drawing::Size(450, 5);
 			this->panel7->TabIndex = 31;
-			// 
-			// pin
-			// 
-			this->pin->AutoSize = true;
-			this->pin->Font = (gcnew System::Drawing::Font(L"Arial Rounded MT Bold", 10));
-			this->pin->ForeColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(110)), static_cast<System::Int32>(static_cast<System::Byte>(84)),
-				static_cast<System::Int32>(static_cast<System::Byte>(181)));
-			this->pin->Location = System::Drawing::Point(23, 210);
-			this->pin->Name = L"pin";
-			this->pin->Size = System::Drawing::Size(38, 20);
-			this->pin->TabIndex = 27;
-			this->pin->Text = L"PIN";
-			// 
-			// pinInput
-			// 
-			this->pinInput->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(43)), static_cast<System::Int32>(static_cast<System::Byte>(39)),
-				static_cast<System::Int32>(static_cast<System::Byte>(56)));
-			this->pinInput->BorderStyle = System::Windows::Forms::BorderStyle::None;
-			this->pinInput->Font = (gcnew System::Drawing::Font(L"Arial Rounded MT Bold", 10.2F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
-				static_cast<System::Byte>(0)));
-			this->pinInput->ForeColor = System::Drawing::Color::White;
-			this->pinInput->Location = System::Drawing::Point(27, 241);
-			this->pinInput->MaxLength = 15;
-			this->pinInput->Name = L"pinInput";
-			this->pinInput->Size = System::Drawing::Size(200, 20);
-			this->pinInput->TabIndex = 26;
-			this->pinInput->UseSystemPasswordChar = true;
-			// 
-			// nominalSavings
-			// 
-			this->nominalSavings->AutoSize = true;
-			this->nominalSavings->Font = (gcnew System::Drawing::Font(L"Arial Rounded MT Bold", 10));
-			this->nominalSavings->ForeColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(110)), static_cast<System::Int32>(static_cast<System::Byte>(84)),
-				static_cast<System::Int32>(static_cast<System::Byte>(181)));
-			this->nominalSavings->Location = System::Drawing::Point(23, 132);
-			this->nominalSavings->Name = L"nominalSavings";
-			this->nominalSavings->Size = System::Drawing::Size(293, 20);
-			this->nominalSavings->TabIndex = 25;
-			this->nominalSavings->Text = L"Nominal uang yang ingin di tabung";
-			// 
-			// addSavingsInput
-			// 
-			this->addSavingsInput->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(43)), static_cast<System::Int32>(static_cast<System::Byte>(39)),
-				static_cast<System::Int32>(static_cast<System::Byte>(56)));
-			this->addSavingsInput->BorderStyle = System::Windows::Forms::BorderStyle::None;
-			this->addSavingsInput->Font = (gcnew System::Drawing::Font(L"Arial Rounded MT Bold", 10.2F, System::Drawing::FontStyle::Regular,
-				System::Drawing::GraphicsUnit::Point, static_cast<System::Byte>(0)));
-			this->addSavingsInput->ForeColor = System::Drawing::Color::White;
-			this->addSavingsInput->Location = System::Drawing::Point(27, 163);
-			this->addSavingsInput->MaxLength = 100;
-			this->addSavingsInput->Name = L"addSavingsInput";
-			this->addSavingsInput->Size = System::Drawing::Size(450, 20);
-			this->addSavingsInput->TabIndex = 24;
 			// 
 			// customerAccNum
 			// 
@@ -240,6 +182,113 @@ namespace GoBanking {
 			this->withdrw->TabIndex = 5;
 			this->withdrw->Text = L"Tambah Tabungan";
 			// 
+			// nominalInputPanel
+			// 
+			this->nominalInputPanel->Controls->Add(this->label2);
+			this->nominalInputPanel->Controls->Add(this->nominalSavings);
+			this->nominalInputPanel->Controls->Add(this->addSavingsInput);
+			this->nominalInputPanel->Controls->Add(this->panel1);
+			this->nominalInputPanel->Location = System::Drawing::Point(27, 138);
+			this->nominalInputPanel->Margin = System::Windows::Forms::Padding(3, 2, 3, 2);
+			this->nominalInputPanel->Name = L"nominalInputPanel";
+			this->nominalInputPanel->Size = System::Drawing::Size(460, 70);
+			this->nominalInputPanel->TabIndex = 42;
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->Font = (gcnew System::Drawing::Font(L"Arial Rounded MT Bold", 10));
+			this->label2->ForeColor = System::Drawing::Color::White;
+			this->label2->Location = System::Drawing::Point(0, 31);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(37, 20);
+			this->label2->TabIndex = 34;
+			this->label2->Text = L"Rp.";
+			// 
+			// nominalSavings
+			// 
+			this->nominalSavings->AutoSize = true;
+			this->nominalSavings->Font = (gcnew System::Drawing::Font(L"Arial Rounded MT Bold", 10));
+			this->nominalSavings->ForeColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(110)), static_cast<System::Int32>(static_cast<System::Byte>(84)),
+				static_cast<System::Int32>(static_cast<System::Byte>(181)));
+			this->nominalSavings->Location = System::Drawing::Point(0, 0);
+			this->nominalSavings->Name = L"nominalSavings";
+			this->nominalSavings->Size = System::Drawing::Size(164, 20);
+			this->nominalSavings->TabIndex = 25;
+			this->nominalSavings->Text = L"Nominal Tabungan";
+			// 
+			// addSavingsInput
+			// 
+			this->addSavingsInput->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(43)), static_cast<System::Int32>(static_cast<System::Byte>(39)),
+				static_cast<System::Int32>(static_cast<System::Byte>(56)));
+			this->addSavingsInput->BorderStyle = System::Windows::Forms::BorderStyle::None;
+			this->addSavingsInput->Font = (gcnew System::Drawing::Font(L"Arial Rounded MT Bold", 10.2F, System::Drawing::FontStyle::Regular,
+				System::Drawing::GraphicsUnit::Point, static_cast<System::Byte>(0)));
+			this->addSavingsInput->ForeColor = System::Drawing::Color::White;
+			this->addSavingsInput->Location = System::Drawing::Point(44, 31);
+			this->addSavingsInput->Margin = System::Windows::Forms::Padding(3, 2, 3, 2);
+			this->addSavingsInput->MaxLength = 100;
+			this->addSavingsInput->Name = L"addSavingsInput";
+			this->addSavingsInput->Size = System::Drawing::Size(411, 20);
+			this->addSavingsInput->TabIndex = 24;
+			// 
+			// panel1
+			// 
+			this->panel1->BackColor = System::Drawing::Color::White;
+			this->panel1->Location = System::Drawing::Point(4, 62);
+			this->panel1->Margin = System::Windows::Forms::Padding(3, 2, 3, 2);
+			this->panel1->Name = L"panel1";
+			this->panel1->Size = System::Drawing::Size(451, 5);
+			this->panel1->TabIndex = 33;
+			// 
+			// pinInputPanel
+			// 
+			this->pinInputPanel->Controls->Add(this->pinLabel);
+			this->pinInputPanel->Controls->Add(this->pinInput);
+			this->pinInputPanel->Controls->Add(this->panel2);
+			this->pinInputPanel->Location = System::Drawing::Point(27, 230);
+			this->pinInputPanel->Margin = System::Windows::Forms::Padding(3, 2, 3, 2);
+			this->pinInputPanel->Name = L"pinInputPanel";
+			this->pinInputPanel->Size = System::Drawing::Size(211, 70);
+			this->pinInputPanel->TabIndex = 43;
+			// 
+			// pinLabel
+			// 
+			this->pinLabel->AutoSize = true;
+			this->pinLabel->Font = (gcnew System::Drawing::Font(L"Arial Rounded MT Bold", 10));
+			this->pinLabel->ForeColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(110)), static_cast<System::Int32>(static_cast<System::Byte>(84)),
+				static_cast<System::Int32>(static_cast<System::Byte>(181)));
+			this->pinLabel->Location = System::Drawing::Point(0, 0);
+			this->pinLabel->Name = L"pinLabel";
+			this->pinLabel->Size = System::Drawing::Size(38, 20);
+			this->pinLabel->TabIndex = 27;
+			this->pinLabel->Text = L"PIN";
+			// 
+			// pinInput
+			// 
+			this->pinInput->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(43)), static_cast<System::Int32>(static_cast<System::Byte>(39)),
+				static_cast<System::Int32>(static_cast<System::Byte>(56)));
+			this->pinInput->BorderStyle = System::Windows::Forms::BorderStyle::None;
+			this->pinInput->Font = (gcnew System::Drawing::Font(L"Arial Rounded MT Bold", 10.2F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->pinInput->ForeColor = System::Drawing::Color::White;
+			this->pinInput->Location = System::Drawing::Point(5, 31);
+			this->pinInput->Margin = System::Windows::Forms::Padding(3, 2, 3, 2);
+			this->pinInput->MaxLength = 15;
+			this->pinInput->Name = L"pinInput";
+			this->pinInput->Size = System::Drawing::Size(200, 20);
+			this->pinInput->TabIndex = 26;
+			this->pinInput->UseSystemPasswordChar = true;
+			// 
+			// panel2
+			// 
+			this->panel2->BackColor = System::Drawing::Color::White;
+			this->panel2->Location = System::Drawing::Point(5, 62);
+			this->panel2->Margin = System::Windows::Forms::Padding(3, 2, 3, 2);
+			this->panel2->Name = L"panel2";
+			this->panel2->Size = System::Drawing::Size(200, 5);
+			this->panel2->TabIndex = 34;
+			// 
 			// AddSavingsFunds
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
@@ -255,6 +304,10 @@ namespace GoBanking {
 			this->addSavingsMainPanel->PerformLayout();
 			this->transferInputPanel->ResumeLayout(false);
 			this->transferInputPanel->PerformLayout();
+			this->nominalInputPanel->ResumeLayout(false);
+			this->nominalInputPanel->PerformLayout();
+			this->pinInputPanel->ResumeLayout(false);
+			this->pinInputPanel->PerformLayout();
 			this->ResumeLayout(false);
 
 		}
